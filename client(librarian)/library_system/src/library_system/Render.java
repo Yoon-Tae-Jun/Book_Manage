ac171b4b9c2b2fd9ad8f83cfd266b91af80e6085
kimgeona <rtguna@icloud.com>
2022-06-12 13:10:34 +0900

    load Book data and Users data 2

--- client(librarian)/library_system/src/library_system/Render.java
+++ client(librarian)/library_system/src/library_system/Render.java
@@ -3,13 +3,7 @@ package library_system;
 import javax.swing.JRadioButton;
 
 import API.APIMethod;
-import library_system.eventListener.ChangeCenterBox;
-import library_system.eventListener.GoToMain;
-import library_system.eventListener.GoToMain2;
-import library_system.eventListener.Login_EventListener;
-import library_system.eventListener.TableToBookData;
-import library_system.eventListener.TableToUserData;
-import library_system.eventListener.main1SearchListener;
+import library_system.eventListener.*;
 import library_system.layout.GuiLibraryDesk;
 import library_system.layout.GuiLoginPage;
 
@@ -31,14 +25,20 @@ public class Render {
 	}
 
 	public void Change_screen() {
+		// 창 전환
 		desk.btn_libraryName.addMouseListener(new GoToMain(desk));
 		desk.btn_myAccount.addMouseListener(new GoToMain2(desk));
+		// CenterBox 창 전환
 		desk.btn_1.addMouseListener(new ChangeCenterBox(desk));
 		desk.btn_2.addMouseListener(new ChangeCenterBox(desk));
 		desk.btn_3.addMouseListener(new ChangeCenterBox(desk));
+		// 책, 사용자 검색
+		desk.btn_search.addMouseListener(new SearchData(desk));
+		desk.btn_search_user.addMouseListener(new SearchData(desk));
+		// JTable 데이터 띄우기
 		desk.table_1.addMouseListener(new TableToBookData(desk));
 		desk.table_2.addMouseListener(new TableToUserData(desk));
-
+		
 	}
 	
 	//getter, setter
